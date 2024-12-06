502f5918a171c258a42d7542304d1210ae91b041
Udita-bit <uditasingh2004@gmail.com>
2024-12-06 23:30:11 +0530

    add files

--- todo/components/todolist.jsx
+++ todo/components/todolist.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import TodoCard from 'TodoCard'
-export default function todolist(props){
-    const{todos}=props
+export default function Todolist(props){
+    const {todos}=props
 
   return (
     <ul className='main'>
         {todos.map((todo,todoIndex)=>{
             return(
-                <TodoCard key={TodoIndex}>
+                <TodoCard{...props} key={TodoIndex}>
                     <p>{todo}</p>
                 </TodoCard>
                     
--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -2,26 +2,32 @@ import { useState } from "react"
 import TodoInput from "./TodoInput"
 import Todolist from "./Todolist"
 function App() {
+  const[todos,setTodos]=useState([])
  
 
 
-const[todos,setTodos]=useState([
-  'Go to the gym',
-    'Eat more fruits and vegetables',
-    'Pick up the kids from school'
 
-]
-);
+
 function handeAddTodos(newTodo){
   const newTodoList=[...todos,newTodo]
   setTodos(newTodoList)
+}
+function handleDeleteTodo(index){
+  const newTodoList=todos.filter((todo,todoIndex)=>{
+    return todoIndex!==index
+  })
+  setTodos(newTodoList)
+
+}
+function handleEditTodo(index){
+
 }
  
 
   return (
     <>
-      <TodoInput setTodos={setTodos}/>
-      <Todolist todos={todos}/>
+      <TodoInput handleAddTodos={handleAddTodos}/>
+      <Todolist handleDeleteTodo={handleDeleteTodo} todos={todos}/>
     </>
   );
 }
